3533c935cfdbe9241a1c1bccf2b64733b7f75d52
kirillziganov <zhiganov_k.n@mail.ru>
2023-09-23 08:16:13 +0300

    Попробовал на практике стайл компонентс

--- src/App.js
+++ src/App.js
@@ -1,3 +1,13 @@
+import styled from 'styled-components';
+
+const Div = styled.div`
+  text-align: center;
+`;
+
 export const App = () => {
-  return <div></div>;
+  return (
+    <Div>
+      <div>123</div>
+    </Div>
+  );
 };
