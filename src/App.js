f44baeed43b9bb1e68140b4b188e765470e86947
kirillziganov <zhiganov_k.n@mail.ru>
2023-09-23 07:57:56 +0300

    Инициализация проекта

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import { useState } from 'react';
+import style from './App.module.css';
+import { CellField } from './components/Stateless/CellField';
+import { StructuralComponent } from './components/StructuralComponent';
+
+export const App = () => {
+  return (
+    <div className={style.app}></div>
+  );
+};
