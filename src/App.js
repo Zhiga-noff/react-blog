dc730032d19eba8d383eddc37a6605a530ffdca1
kirillziganov <zhiganov_k.n@mail.ru>
2023-09-23 08:06:18 +0300

    Установка пакетов

--- src/App.js
+++ src/App.js
@@ -1,10 +1,3 @@
-import { useState } from 'react';
-import style from './App.module.css';
-import { CellField } from './components/Stateless/CellField';
-import { StructuralComponent } from './components/StructuralComponent';
-
 export const App = () => {
-  return (
-    <div className={style.app}></div>
-  );
+  return <div></div>;
 };
