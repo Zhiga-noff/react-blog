15a4cbbf922ab17e4d28dce57ccafa351c1b4c2c
user <zhiganov_k.n@mail.ru>
2023-09-24 19:36:36 +0300

    Начал верстать шляпу в проекте

--- src/Blog.jsx
+++ src/Blog.jsx
@@ -9,18 +9,31 @@ const H2 = styled.h2`
   text-align: center;
 `;
 
-export const Header = () => {
-  return <Content>Шапка</Content>;
+const AppColumn = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  width: 1000px;
+  min-height: 100%;
+  background-color: white;
+  margin: 0 auto;
+`;
+
+export const Header = ({ className }) => {
+  return <header className={className}>Шапка</header>;
 };
 
+const StyledHeader = styled(Header)`
+  height: 120px;
+`;
 export const Footer = () => {
-  return <Content>Футтер</Content>;
+  return <footer>Футтер</footer>;
 };
 
 export const Blog = () => {
   return (
-    <>
-      <Header />
+    <AppColumn>
+      <StyledHeader />
 
       <Content>
         <H2>Контент страницы</H2>
@@ -35,6 +48,6 @@ export const Blog = () => {
         </Routes>
       </Content>
       <Footer />
-    </>
+    </AppColumn>
   );
 };
